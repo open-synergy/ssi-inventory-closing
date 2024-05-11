8889a95487fd561172a10ca06aaae89cc2c2cb01
Andhitia Rama <andhitia.r@gmail.com>
2024-05-11 22:04:07 +0700

    [UPD] ssi_inventory_closing_queue
    * Overhaul terkait penggunaan mixin baru

--- ssi_inventory_closing_queue/models/inventory_closing.py
+++ ssi_inventory_closing_queue/models/inventory_closing.py
@@ -3,85 +3,45 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-from odoo import _, api, fields, models
+from odoo import _, api, models
 
 from odoo.addons.ssi_decorator import ssi_decorator
 
 
 class InventoryClosing(models.Model):
-    _inherit = "inventory_closing"
+    _inherit = [
+        "mixin.transaction_queue_cancel",
+        "mixin.transaction_queue_done",
+        "inventory_closing",
+    ]
+    _name = "inventory_closing"
 
-    _after_approved_method = False
+    _after_approved_method = "action_queue_done"
+    _statusbar_visible_label = "draft,confirm,queue_done,done"
 
-    queue_job_batch_ids = fields.One2many(
-        string="Queue Job Batch(s)",
-        comodel_name="inventory_closing_queue_job_batch",
-        inverse_name="inventory_closing_id",
-    )
+    # Attributes related to add element on view automatically
+    _automatically_insert_view_element = True
+    _automatically_insert_multiple_approval_page = True
+    _queue_processing_create_page = True
+    _automatically_insert_queue_done_button = False
+    _automatically_insert_queue_cancel_button = False
 
-    @api.depends(
-        "queue_job_batch_ids",
-        "queue_job_batch_ids.job_batch_state",
-    )
-    def _compute_all_job_finished(self):
-        for rec in self:
-            job_not_finish_ids = rec.queue_job_batch_ids.filtered(
-                lambda x: x.job_batch_state != "finished"
-            )
-            if len(job_not_finish_ids) > 0:
-                rec.all_job_finished = False
-            else:
-                rec.all_job_finished = True
+    _queue_to_done_insert_form_element_ok = True
+    _queue_to_done_form_xpath = "//group[@name='queue_processing']"
 
-    all_job_finished = fields.Boolean(
-        string="All Job Finished",
-        compute="_compute_all_job_finished",
-        store=True,
-    )
+    _queue_to_cancel_insert_form_element_ok = True
+    _queue_to_cancel_form_xpath = "//group[@name='queue_processing']"
 
-    def _check_all_finished(self):
+    _method_to_run_from_wizard = "action_queue_cancel"
+
+    @ssi_decorator.post_queue_done_action()
+    def _02_create_aml_from_svl_on_queue_done(self):
         self.ensure_one()
-        result = True
-        for job_batch in self.queue_job_batch_ids:
-            if job_batch.job_batch_state != "finished":
-                result = False
-                break
-        return result
-
-    def _prepare_ic_queue_job_batch_data(self, batch_id):
-        data = {
-            "inventory_closing_id": self.id,
-            "job_batch_id": batch_id,
-        }
-        return data
-
-    def create_ic_queue_job_batch(self, batch_id):
-        self.ensure_one()
-        obj_ic_queue_job_batch = self.env["inventory_closing_queue_job_batch"]
-        obj_ic_queue_job_batch.create(self._prepare_ic_queue_job_batch_data(batch_id))
-        return True
-
-    def _create_aml_from_svl(self, svl_ids, index):
-        self.ensure_one()
-        queue_job_obj = self.env["queue.job"]
-        str_group = "[%s] Inventory Closing Batch for ID %s" % (str(index), self.id)
-        batch = self.env["queue.job.batch"].get_new_batch(str_group)
-        for svl in svl_ids:
-            description = "Create AML From SVL for ID %s" % (self.id)
-            job = (
-                svl.with_context(job_batch=batch)
-                .with_delay(description=_(description))
-                ._create_accounting_entry()
-            )
-            criteria = [("uuid", "=", job.uuid)]
-            job_id = queue_job_obj.search(criteria, limit=1, order="id desc").id
-            svl.update(
-                {
-                    "job_id": job_id,
-                }
-            )
-        batch.enqueue()
-        self.create_ic_queue_job_batch(batch.id)
+        for svl in self.stock_valuation_layer_ids:
+            description = "Create AML From SVL ID %s" % (svl.id)
+            svl.with_context(job_batch=self.done_queue_job_batch_id).with_delay(
+                description=_(description)
+            )._create_accounting_entry()
 
     @ssi_decorator.post_done_action()
     def _01_update_svl_journal(self):
@@ -93,8 +53,8 @@ class InventoryClosing(models.Model):
         self.ensure_one()
         return True
 
-    @ssi_decorator.post_approve_action()
-    def _01_post_update_svl_journal(self):
+    @ssi_decorator.post_queue_done_action()
+    def _01_update_svl_journal_on_queue_done(self):
         self.ensure_one()
         for move in self.stock_move_ids:
             if not move.journal_id:
@@ -104,26 +64,26 @@ class InventoryClosing(models.Model):
                     }
                 )
 
-    @ssi_decorator.post_approve_action()
-    def _02_post_create_aml_from_svl(self):
+    @ssi_decorator.post_cancel_action()
+    def _01_delete_aml(self):
         self.ensure_one()
-        svl_ids = self.stock_valuation_layer_ids.filtered(lambda x: not x.job_id)
-        if svl_ids:
-            batch_job_limit = self.type_id.batch_job_limit
-            for index, i in enumerate(range(0, len(svl_ids), batch_job_limit), start=1):
-                self._create_aml_from_svl(svl_ids[i : i + batch_job_limit], index)
-        else:
-            self._requeue_job_batch()
+        return True
 
-    def _requeue_job_batch(self):
+    @ssi_decorator.post_queue_cancel_action()
+    def _01_delete_aml_on_queue_cancel(self):
         self.ensure_one()
-        svl_ids = self.stock_valuation_layer_ids.filtered(
-            lambda x: x.job_state != "done"
-        )
-        if svl_ids:
-            for svl in svl_ids:
-                svl.job_id.requeue()
+        for svl in self.stock_valuation_layer_ids:
+            description = "Delete AML From SVL ID %s" % (svl.id)
+            svl.with_context(job_batch=self.cancel_queue_job_batch_id).with_delay(
+                description=_(description)
+            )._delete_accounting_entry()
 
-    def action_requeue(self):
-        for record in self.sudo():
-            record._requeue_job_batch()
+    @api.model
+    def _get_policy_field(self):
+        res = super(InventoryClosing, self)._get_policy_field()
+        policy_field = [
+            "queue_done_ok",
+            "queue_cancel_ok",
+        ]
+        res += policy_field
+        return res
